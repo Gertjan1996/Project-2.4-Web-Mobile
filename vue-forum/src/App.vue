926b540caa4da8b0a0ae49f1c3e68d2d89e5f52d
Gertjan <gertjanhaan@live.nl>
2020-06-04 19:14:46 +0200

    Nieuw project met Vuetify
    Nieuw project opgezet om Vuetify vanaf begin goed te implementeren. Inlog logica vanaf ander project later overzetten nog.

---
+++ vue-forum/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-app>
+    <v-toolbar dark>
+      <v-app-bar-nav-icon @click.native.stop="sideNav = !sideNav"></v-app-bar-nav-icon>
+      <v-toolbar-title>Sportforum</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn text>
+          <v-icon left>mdi-login</v-icon>
+          Inloggen
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-navigation-drawer v-model="sideNav">
+      <v-list>
+        <v-list-item>
+          <v-list-item-action>
+            <v-icon>mdi-login</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>Inloggen</v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <main>
+    </main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  components: {
+    //
+  },
+
+  data: () => ({
+    sideNav: false
+  })
+}
+</script>
