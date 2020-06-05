cef601df4d58c0d1547dd65e1ed57e52aed16569
Alreadydo <33254518+Alreadydo@users.noreply.github.com>
2020-06-06 01:32:18 +0200

    registratie link

--- ForumApplication/app/src/main/java/com/example/forumapplication/LoginActivity.java
+++ ForumApplication/app/src/main/java/com/example/forumapplication/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.forumapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,4 +12,9 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    public void Registeren(View view){
+
+    }
+
 }
