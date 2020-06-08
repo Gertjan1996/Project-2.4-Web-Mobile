66cda0bce2970c7e64af322b32d32c47dec2b93e
Alreadydo <33254518+Alreadydo@users.noreply.github.com>
2020-06-08 16:11:10 +0200

    login, registratie en logout
    werkt compleet

--- ForumApplication/app/src/main/java/com/example/forumapplication/MainActivity.java
+++ ForumApplication/app/src/main/java/com/example/forumapplication/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.forumapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +15,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void logout(View view){
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+        finish();
+    }
+
 }
--- ForumApplication/app/src/main/java/com/example/forumapplication/RegistrateActivity.java
+++ ForumApplication/app/src/main/java/com/example/forumapplication/RegistrateActivity.java
@@ -1,14 +1,20 @@
 package com.example.forumapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrateActivity extends AppCompatActivity {
@@ -25,8 +31,15 @@ public class RegistrateActivity extends AppCompatActivity {
         mWachtwoord = findViewById(R.id.wachtwoord);
         mWachtwoord_confire = findViewById(R.id.confirewachtwoord);
         registratieButton = findViewById(R.id.Registratie_Button);
+
         fAuth = FirebaseAuth.getInstance();
         progressBar = findViewById(R.id.progressBar);
+
+        //controleer of de gebruiker al ingelogd of geregistreerd is
+        if(fAuth.getCurrentUser()!= null){
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+        }
 
         registratieButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,6 +64,19 @@ public class RegistrateActivity extends AppCompatActivity {
                     return;
                 }
                 progressBar.setVisibility(View.VISIBLE);
+
+                //Registreren de gebruiker in de firebase
+                fAuth.createUserWithEmailAndPassword(email,wachtwoord).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(RegistrateActivity.this, "Uw account is geregistreerd",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        }else{
+                            Toast.makeText(RegistrateActivity.this, "Foutmelding!"+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
         });
     }
