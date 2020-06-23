7964bfe6c82ba76c3348d24b1ed0093453ca1fc6
Gertjan <gertjanhaan@live.nl>
2020-06-23 17:21:50 +0200

    Middleware + post en delete methods
    Middleware toegevoegd om JSON mee te kunnen sturen in een post request, als payload. Verder CORS middleware toegevoegd om CORS toe te staan, daarnaast post methods en delete methods toegevoegd

--- node-API/src/index.js
+++ node-API/src/index.js
@@ -1,11 +1,21 @@
 import 'dotenv/config' // Environment variables (should come before other local imports)
 import cors from 'cors'
 import express from 'express'
+import { v4 } from 'uuid'
 
 console.log('Hello Node.js Project')
 console.log('SECRET: ' + process.env.MY_SECRET)
 
 const app = express() // Express application instance
+
+app.use(express.json()) // Middleware to transform json body type from request object
+app.use(express.urlencoded({ extended: true })) // Middleware to access payload of an HTTP POST request
+app.use(cors()) // Middleware to allow Cross-origin resource sharing
+app.use((req, res, next) => { // Middleware to add users[1] as sender and categories[1] as category - only for test purposes
+  req.user = users[1]
+  req.category = categories[1]
+  next()
+})
 
 let users = { // Hardcoded - MongoDB connection in the future
   1: {
@@ -49,10 +59,8 @@ let posts = { // Hardcoded - MongoDB connection in the future
     text: 'Test Post nr. 2 van user met userId 2 in categorie met categoryId 2',
     userId: '2',
     categoryId: '2'
-  },
+  }
 }
-
-app.use(cors()) // Allow Cross-origin resource sharing
 
 app.get('/users', (req, res) => { // curl http://localhost:3000/users
   return res.send(Object.values(users))
@@ -82,12 +90,53 @@ app.get('/posts/:postId', (req, res) => { // curl http://localhost:3000/posts/1
   return res.send(posts[req.params.postId])
 })
 
+app.post('/posts', (req, res) => { // curl -X POST -H "Content-Type:application/json" http://localhost:3000/posts -d "{\"text\":\"Test\"}"
+  const id = v4()
+  const post = {
+    id,
+    text: req.body.text,
+    userId: req.user.id,
+    categoryId: req.category.id
+  }
+  posts[id] = post
+  return res.send(post)
+})
+
+app.delete('/posts/:postId', (req, res) => { // curl -X DELETE http://localhost:3000/posts/1
+  const {
+    [req.params.postId]: post,
+    ...otherPosts
+  } = posts
+  posts = otherPosts
+  return res.send(post)
+})
+
 app.get('/categories', (req, res) => { // curl http://localhost:3000/categories
   return res.send(Object.values(categories))
 })
      
 app.get('/categories/:categoryId', (req, res) => { // curl http://localhost:3000/categories/1
   return res.send(categories[req.params.categoryId])
+})
+
+app.post('/categories', (req, res) => { // curl -X POST -H "Content-Type:application/json" http://localhost:3000/categories -d "{\"category\":\"Testcat\",\"imgPath\":\"Testpat\"}"
+  const id = v4()
+  const category = {
+    id,
+    category: req.body.category,
+    imgPath: req.body.imgPath
+  }
+  categories[id] = category
+  return res.send(category)
+})
+
+app.delete('/categories/:categoryId', (req, res) => { // curl -X DELETE http://localhost:3000/categories/1
+  const {
+    [req.params.categoryId]: category,
+    ...otherCategories
+  } = categories
+  categories = otherCategories
+  return res.send(category)
 })
 
 app.listen(process.env.PORT, () =>
