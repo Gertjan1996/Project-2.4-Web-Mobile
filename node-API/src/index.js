bb2c4f3841657ac239758defd75b0340f251141d
Gertjan <gertjanhaan@live.nl>
2020-06-23 15:16:44 +0200

    Added CORS + Express
    CORS middleware dat automatisch CORS headers toevoegd aan http-requests en Express server toegevoegd (populairste web application framework icm met node om server applicaties te bouwen.). Verder PORT naar .env file verplaatst.

--- node-API/src/index.js
+++ node-API/src/index.js
@@ -1,5 +1,18 @@
-import 'dotenv/config' // Environment variables (should come before local imports)
+import 'dotenv/config' // Environment variables (should come before other local imports)
+import cors from 'cors'
+import express from 'express'
 
-console.log('Hello Node.js Project.')
+console.log('Hello Node.js Project')
+console.log('SECRET: ' + process.env.MY_SECRET)
 
-console.log(process.env.MY_SECRET)
+const app = express() // Express application instance
+
+app.use(cors()) // Allow Cross-origin resource sharing
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(process.env.PORT, () =>
+  console.log(`API draait op poort ${process.env.PORT}!`) // App listens on port set in .env
+)
