6b56935e4593315e3bf992d0d27ac4c8a0bb44db
Gertjan <gertjanhaan@live.nl>
2020-06-28 12:08:44 +0200

    getter for all posts toegevoegd aan API

--- node-API/src/routes/index.js
+++ node-API/src/routes/index.js
@@ -1,9 +1,11 @@
 import user from './user'
 import category from './category'
 import post from './post'
+import post_all from './post_all'
  
 export default {
   user,
   category,
   post,
+  post_all
 }
